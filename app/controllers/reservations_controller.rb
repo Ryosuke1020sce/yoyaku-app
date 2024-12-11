1f1106facddb84ebfb4d52082517abefd36d1c03
Ryosuke1020sce <lickey622saito@gmail.com>
2024-12-11 18:47:34 +0900

    予約枠の一覧表示機能の実装

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,7 @@ class ReservationsController < ApplicationController
 
   def index
     @store = Store.find(params[:store_id])
+    @reservations = Reservation.where(store_id: params[:store_id])
   end
 
   def new
@@ -11,12 +12,8 @@ class ReservationsController < ApplicationController
   def create
 
     @reservation = Reservation.new(reservation_params)
-    
+
     if @reservation.save
-      n = params[:reservation][:rsv_n].to_i - 1
-      n.times do
-        @reservation = Reservation.create(reservation_params)
-      end
       redirect_to store_reservations_path(params[:store_id])
     else
       render :new, status: :unprocessable_entity
@@ -26,7 +23,7 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:rsv_date, :rsv_time_id,
+    params.require(:reservation).permit(:rsv_date, :rsv_time_id, :rsv_n, :explain,
       :service_type_id, :upper_people_n, :fee).merge(store_id: params[:store_id])
   end
 
