a0acad1d978216e0701dc0ae092cf2057bb29f68
Ryosuke1020sce <lickey622saito@gmail.com>
2024-12-20 15:55:52 +0900

    自動カレンダー作成機能

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -34,6 +34,7 @@ class StoresController < ApplicationController
   def show
     @calendar = Calendar.new
     @calendars = Calendar.where(store_id: params[:id])
+    check_calendar
   end
 
   def edit
@@ -62,6 +63,53 @@ class StoresController < ApplicationController
   
   def set_store
     @store = Store.find(params[:id])
+  end
+  
+  def next_month(m)
+    if m == 12
+      return 1
+    else
+      return m + 1
+    end
+  end
+
+  def next_year(y, m)
+    if m == 12
+      return y + 1
+    else
+      return y
+    end
+  end
+
+  def check_calendar
+
+    today = Date.today
+    today_m = today.month
+    today_y = today.year
+    d1 = Date.new(today_y, today_m, 1)
+    next_y_1 = next_year(today_y, today_m) 
+    next_m_1 = next_month(today_m)
+    d2 = Date.new(next_y_1, next_m_1, 1)
+    next_y_2 = next_year(next_y_1, next_m_1) 
+    next_m_2 = next_month(next_m_1)
+    d3 = Date.new(next_y_2, next_m_2, 1)
+
+    boo1 = (Calendar.where(store_id: @store.id, std_date: d1).count != 0)
+    boo2 = (Calendar.where(store_id: @store.id, std_date: d2).count != 0)
+    boo3 = (Calendar.where(store_id: @store.id, std_date: d3).count != 0)
+
+    if !boo1
+      Calendar.create(std_date: d1, store_id: @store.id)
+    end  
+
+    if !boo2
+      Calendar.create(std_date: d2, store_id: @store.id)
+    end  
+
+    if !boo3
+      Calendar.create(std_date: d3, store_id: @store.id)
+    end  
+
   end
 
 end
