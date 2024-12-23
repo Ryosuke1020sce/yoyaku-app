0b9fc20673fae2d897a6397376becb7b987ed456
Ryosuke1020sce <lickey622saito@gmail.com>
2024-12-23 15:01:35 +0900

    Googleフォントの導入

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ApplicationRecord
 
   belongs_to :store
   belongs_to :rsv_group, optional: true
-  has_many :guests
+  has_many :guests, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :rsv_time
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,8 +1,8 @@
 class Store < ApplicationRecord
 
   belongs_to :user
-  has_many :reservations
-  has_many :calendars
+  has_many :reservations, dependent: :destroy
+  has_many :calendars, dependent: :destroy
   has_many :rsv_groups, through: :calendars
 
   has_one_attached :image
